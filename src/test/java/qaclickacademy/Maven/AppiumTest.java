4fceada2a6b3393f3759a68177bb237d4373c242
Bakul Karmakar <bakulkarmakar9@gmail.com>
2022-09-06 21:33:33 +0530

    first commit

---
+++ src/test/java/qaclickacademy/Maven/AppiumTest.java
@@ -0,0 +1,21 @@
+package qaclickacademy.Maven;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+	
+	@Test
+	
+	public void Appium()
+	{
+		System.out.println("Appium Test Case");
+		
+	}
+	
+	@Test
+	public void AppTest2()
+	{
+		System.out.println("Appium Test case 2");
+	}
+
+}
---
+++ src/test/java/qaclickacademy/Maven/SeleniumTest.java
@@ -0,0 +1,20 @@
+package qaclickacademy.Maven;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+	
+	@Test
+	
+	public void SeleniumTestCase()
+	{
+		System.out.println("SeleniumTestCase");
+	}
+
+	@Test
+	
+	public void TestCase2()
+	{
+		System.out.println("testcase2");
+	}
+}
